d93cf0b043d8d7739f42813bbca26ad1c36e4bea
lilliangonick <lillygonick@gmail.com>
2025-04-26 00:25:55 -0700

    fixed port number

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ dotenv.config();
 
 const app = express(); // initalizes express app 
 
-const PORT = process.env.PORT || 000;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
